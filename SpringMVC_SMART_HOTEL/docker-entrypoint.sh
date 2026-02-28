1a23aa99ad55e3dda518a9bde3ea880302f1b3fd
unknown <haopham081098@gmail.com>
2026-02-28 22:49:15 +0700

    remove else

--- SpringMVC_SMART_HOTEL/docker-entrypoint.sh
+++ SpringMVC_SMART_HOTEL/docker-entrypoint.sh
@@ -1,33 +1,36 @@
 #!/bin/sh
 set -e
+
 # Railway/Render cung cấp biến PORT
 if [ -n "$PORT" ]; then
   sed -i "s/8080/$PORT/g" /usr/local/tomcat/conf/server.xml
 fi
-# Cấu hình từ biến môi trường
+
 CATALINA_OPTS="$CATALINA_OPTS"
+
 [ -n "$FRONTEND_BASE_URL" ] && CATALINA_OPTS="$CATALINA_OPTS -Dfrontend.baseUrl=$FRONTEND_BASE_URL"
 [ -n "$BACKEND_BASE_URL" ] && CATALINA_OPTS="$CATALINA_OPTS -Dvnpay.returnUrl=$BACKEND_BASE_URL/SpringMVC_SMART_HOTEL/api/callback/vnpay -Dmomo.redirectUrl=$BACKEND_BASE_URL/SpringMVC_SMART_HOTEL/api/callback/momo/guest -Dmomo.ipnUrl=$BACKEND_BASE_URL/SpringMVC_SMART_HOTEL/api/callback/momo"
+
 [ -n "$GEMINI_API_KEY" ] && CATALINA_OPTS="$CATALINA_OPTS -Dgemini.api.key=$GEMINI_API_KEY"
 [ -n "$MOMO_PARTNER_CODE" ] && CATALINA_OPTS="$CATALINA_OPTS -Dmomo.partnerCode=$MOMO_PARTNER_CODE"
 [ -n "$MOMO_ACCESS_KEY" ] && CATALINA_OPTS="$CATALINA_OPTS -Dmomo.accessKey=$MOMO_ACCESS_KEY"
 [ -n "$MOMO_SECRET_KEY" ] && CATALINA_OPTS="$CATALINA_OPTS -Dmomo.secretKey=$MOMO_SECRET_KEY"
 [ -n "$VNPAY_TMN_CODE" ] && CATALINA_OPTS="$CATALINA_OPTS -Dvnpay.tmnCode=$VNPAY_TMN_CODE"
 [ -n "$VNPAY_HASH_SECRET" ] && CATALINA_OPTS="$CATALINA_OPTS -Dvnpay.hashSecret=$VNPAY_HASH_SECRET"
-# Railway MySQL: dùng MYSQLHOST,... nếu có; không thì dùng HIBERNATE_CONNECTION_*
+
+# Railway MySQL
 if [ -n "$MYSQLHOST" ]; then
   _db="${MYSQL_DATABASE_OVERRIDE:-$MYSQLDATABASE}"
   _url="jdbc:mysql://${MYSQLHOST}:${MYSQLPORT:-3306}/${_db}?allowPublicKeyRetrieval=TRUE&useSSL=TRUE&serverTimezone=UTC"
 
-  CATALINA_OPTS="$CATALINA_OPTS"
   CATALINA_OPTS="$CATALINA_OPTS -Dhibernate.connection.url=$_url"
   CATALINA_OPTS="$CATALINA_OPTS -Dhibernate.connection.username=$MYSQLUSER"
   CATALINA_OPTS="$CATALINA_OPTS -Dhibernate.connection.password=$MYSQLPASSWORD"
-fi
 else
   [ -n "$HIBERNATE_CONNECTION_URL" ] && CATALINA_OPTS="$CATALINA_OPTS -Dhibernate.connection.url=$HIBERNATE_CONNECTION_URL"
   [ -n "$HIBERNATE_CONNECTION_USERNAME" ] && CATALINA_OPTS="$CATALINA_OPTS -Dhibernate.connection.username=$HIBERNATE_CONNECTION_USERNAME"
   [ -n "$HIBERNATE_CONNECTION_PASSWORD" ] && CATALINA_OPTS="$CATALINA_OPTS -Dhibernate.connection.password=$HIBERNATE_CONNECTION_PASSWORD"
 fi
+
 export CATALINA_OPTS
 exec catalina.sh run
